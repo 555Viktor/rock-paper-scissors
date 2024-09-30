0098110d340c2ba177123036925663ab4d0e3c9e
555Viktor <victor.vaskov2002@gmail.com>
2024-09-30 14:11:41 +0300

    Make user prompt loop until valid option selected

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let userScore = 0;
 let computerScore = 0;
 let draws = 0;
 
+let userInput;
+
 let userChoice = getUserChoice();
 let computerChoice = getComputerChoice();
 
@@ -19,40 +21,46 @@ function getComputerChoice () {
 }
 
 function getUserChoice () {
-    let userInput = prompt('Choose one: rock, paper, scissors')
-
-    return userInput.toLowerCase();
+    while(true) {
+        userInput = prompt('Choose one: rock, paper, scissors');
+        
+        if (options.includes(userInput.toLowerCase())) {
+            return userInput.toLowerCase();
+        } else {
+            alert('Please select a valid value');
+        }
+    }
 }
 
 function playRound (userChoice, computerChoice) {
 
     if (userChoice === computerChoice) {
         draws++;
-        alert('Draw!')
-        announceScore()
+        alert('Draw!');
+        announceScore();
     } 
 
     else if (userChoice === 'rock' && computerChoice === 'scissors') {
         userScore++;
-        alert('Player wins!')
-        announceScore()
+        alert('Player wins!');
+        announceScore();
     } 
 
     else if (userChoice === 'paper' && computerChoice === 'rock') {
         userScore++;
-        alert('Player wins!')
-        announceScore()
+        alert('Player wins!');
+        announceScore();
     } 
 
     else if (userChoice === 'scissors' && computerChoice === 'paper') {
         userScore++;
-        alert('Player wins!')
-        announceScore()
+        alert('Player wins!');
+        announceScore();
     } 
     
     else {
         computerScore++;
-        alert('Computer wins!')
-        announceScore()
+        alert('Computer wins!');
+        announceScore();
     }
 }
