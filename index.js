e991610facdd5f9be0f0b2d78aee0130127e07bf
555Viktor <victor.vaskov2002@gmail.com>
2024-09-30 13:57:41 +0300

    Add basic game logic to win and helper alert function

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ let draws = 0;
 let userChoice = getUserChoice();
 let computerChoice = getComputerChoice();
 
+function announceScore () {
+    return alert(`Wins : ${userScore}, Losses : ${computerScore}, Draws : ${draws}`);
+}
+
 function getComputerChoice () {
     let randomIndex = Math.floor(Math.random() * options.length);
 
@@ -20,4 +24,35 @@ function getUserChoice () {
     return userInput.toLowerCase();
 }
 
+function playRound (userChoice, computerChoice) {
 
+    if (userChoice === computerChoice) {
+        draws++;
+        alert('Draw!')
+        announceScore()
+    } 
+
+    else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        userScore++;
+        alert('Player wins!')
+        announceScore()
+    } 
+
+    else if (userChoice === 'paper' && computerChoice === 'rock') {
+        userScore++;
+        alert('Player wins!')
+        announceScore()
+    } 
+
+    else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        userScore++;
+        alert('Player wins!')
+        announceScore()
+    } 
+    
+    else {
+        computerScore++;
+        alert('Computer wins!')
+        announceScore()
+    }
+}
