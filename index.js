623de8ac4ed07fc9f0bf6758f9466a1b15a4ccd3
555Viktor <victor.vaskov2002@gmail.com>
2024-10-08 16:39:24 +0300

    Add basic player and computer choice funcs

--- index.js
+++ index.js
@@ -1,91 +1,57 @@
-// Choice options + userInput
-let options = ['rock', 'paper', 'scissors'];
+// DOM Elements
+let selectBtnsContainer = document.querySelector('.select-rps-container');
+let selectChoiceBtns = document.querySelectorAll('.btnSelectRps');
+let selectChoiceImgs = document.querySelectorAll('.choice-img');
 
-let userInput;
+let playerChoiceImg = document.querySelector('#playerChoiceImg');
+let computerChoiceImg = document.querySelector('#computerChoiceImg')
 
-// Score variables + max rounds
-let userScore = 0;
-let computerScore = 0;
-let draws = 0;
+// Score
+let scoreAnnouceHeader = document.querySelector('#headerAnnounce')
+let playerScoreSpan = document.querySelector('#playerScore');
+let computerScoreSpan = document.querySelector('#computerScore');
 
-let roundNum = 1;
-let maxRounds = 5;
+// Options
+let choicesArr = ['rock', 'paper', 'scissors'];
 
-// Helper function to alert score
-function announceScore () {
-    return alert(`Wins : ${userScore}, Losses : ${computerScore}, Draws : ${draws}`);
-}
+// Computer Choice Funcs
 
-// Functions to get computer and user choice for game to start
 function getComputerChoice () {
-    let randomIndex = Math.floor(Math.random() * options.length);
+    let randomNum = Math.floor(Math.random() * choicesArr.length);
 
-    return options[randomIndex];
+    return choicesArr[randomNum];
 }
 
-function getUserChoice () {
+function displayComputerChoice () {
+    computerChoiceImg.src = `./images/${getComputerChoice()}-icon.png`;
+}
 
-    while (true) {
-        userInput = prompt(`Round #${roundNum}/${maxRounds} | Choose one: rock, paper, scissors`);
+// Player Choice Funcs
+
+function getPlayerChoice () {
+    selectChoiceBtns.forEach(btn => {
+
+        btn.addEventListener('click', () => {
+            let playerChoice = btn.dataset.choice;
+
+            displayPlayerChoice(playerChoice)
+
+            return playerChoice;
+        })
         
-        if (options.includes(userInput.toLowerCase())) {
-            return userInput.toLowerCase();
-        } else {
-            alert('Please select a valid value');
-        }
-    }
+    })
+}
+
+function displayPlayerChoice (choice) {
+    playerChoiceImg.src = `./images/${choice}-icon.png`;
+
+    playerChoiceImg.classList.replace('default-img', 'display-choice-img');
+}
+
+// Decide winner
+
+function decideWinner () {
 
 }
 
-// Function to play 1 round
-function playRound (userSelect, computerSelect) {
-
-    if (userSelect === computerSelect) {
-        draws++;
-        alert(`Draw! \n${userSelect} vs. ${computerSelect}`);
-        announceScore();
-    } 
-
-    else if (userSelect === 'rock' && computerSelect === 'scissors') {
-        userScore++;
-        alert(`Player wins! \n${userSelect} vs. ${computerSelect}`);
-        announceScore();
-    } 
-
-    else if (userSelect === 'paper' && computerSelect=== 'rock') {
-        userScore++;
-        alert(`Player wins! \n${userSelect} vs. ${computerSelect}`);
-        announceScore();
-    } 
-
-    else if (userSelect === 'scissors' && computerSelect === 'paper') {
-        userScore++;
-        alert(`Player wins! \n${userSelect} vs. ${computerSelect}`);
-        announceScore();
-    } 
-    
-    else {
-        computerScore++;
-        alert(`Computer wins! \n${userSelect} vs. ${computerSelect}`);
-        announceScore();
-    }
-}
-
-// Function to play maxRounds
-
-function playGame () {
-
-    for (let i = 1; i <= maxRounds; i++) {
-        playRound (getUserChoice(), getComputerChoice());
-        roundNum++;
-
-        if (i >= maxRounds) {
-            alert(`Max rounds played! Results will be reset.`)
-
-            userScore = computerScore = draws = 0;
-            roundNum = 1;
-        }
-    }
-
-}
-
+getPlayerChoice()
